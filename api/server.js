cffaf0b4cbed6d33b8a6a2b378adc870f9fac7f8
Nicholas Smith <nicholassmithbus@gmail.com>
2021-04-06 13:46:31 -0600

    completed MVP

--- api/server.js
+++ api/server.js
@@ -1,2 +1,11 @@
 // implement your server here
 // require your posts router and connect it here
+const postsRouter = require("./posts/posts-router");
+const express = require("express");
+const server = express();
+
+server.use("/api/posts", postsRouter);
+
+server.use(express.json());
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // require your server and launch it here
+const server = require("./api/server");
+
+const PORT = 5000;
+server.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
